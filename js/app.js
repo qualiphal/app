39f6e7f16698ad35fd4e3914802214de1bddd62e
Anudit Nagar <nagaranudit@gmail.com>
2020-11-07 09:11:48 +0530

    Intergrate Detector 1

--- js/app.js
+++ js/app.js
@@ -22,5 +22,34 @@ async function check(){
     })
     let picture = webcam.snap(); //Base4 of image.
     document.querySelector('#capimg').src = picture;
-    console.log(picture);
+    let file = await dataUrlToFile(picture, 'cap.jpg');
+    var formdata = new FormData();
+    formdata.append("image", file);
+
+    var requestOptions = {
+      method: 'POST',
+      body: formdata,
+      redirect: 'follow'
+    };
+
+    fetch("https://qualiphal-detector1.herokuapp.com/", requestOptions)
+      .then(response => response.json())
+      .then(result => {
+        console.log(result);
+        Swal.fire({
+          icon: 'info',
+          title: 'Result',
+          text: `Our prediction is ${result['freshness']}% fresh.`,
+        })
+      })
+      .catch(error => console.log('error', error));
+
 }
+
+
+async function dataUrlToFile(dataUrl, fileName) {
+  const res = await fetch(dataUrl);
+  const blob = await res.blob();
+  return new File([blob], fileName, { type: 'image/jpg' });
+}
+
